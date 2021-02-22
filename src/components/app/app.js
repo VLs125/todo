adb83b10d08cc6c5695a5448ae1f05b59a363de5
Vladislav Stalmakhovich <VLs125.25@gmail.com>
2021-02-22 16:37:20 +1000

    added delete function

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import AppHeader from '../app-header/app-header'
 import ItemStatusFilter from '../item-status-filter/item-status-filter'
 import SearchPanel from '../search-panel/search-panel'
 import TodoList from '../todo-list/todo-list'
 
-const App =()=>{
-    const todoData = [
+export default class App extends Component{
+    state = {
+    todoData : [
       {label:'First task',important:false, id:1},
       {label:'Second task',important:true,id:2},
       {label:'Third task',important:true, id:3}
     ]
+  };
+
+  deleteItem = (id) =>{
+    this.setState(({todoData})=>{
+      const idx = todoData.findIndex((el)=>el.id===id);
+
+      const newArray = [
+        ...todoData.slice(0, idx), 
+        ...todoData.slice(idx + 1)
+      ]
+
+      return {
+        todoData : newArray
+      }
+    })
+  }
+
+  render(){
     return( <div className="todo-app">
         <AppHeader todo={1} done={3}/>
         <div className="top-panel d-flex">
@@ -17,9 +36,8 @@ const App =()=>{
         <ItemStatusFilter/>
         </div>
         <TodoList
-        onDeleted={(id)=> console.log(id)}
-         todos = {todoData}/>
+        onDeleted={this.deleteItem}
+         todos = {this.state.todoData}/>
       </div>
       )}
-
-      export default App
+    }
