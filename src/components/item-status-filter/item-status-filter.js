35a0bbe476f1755d2d4bf2b109876a888599d1d5
Vladislav Stalmakhovich <VLs125.25@gmail.com>
2021-03-31 22:23:56 +1000

    refactoring to typescript, redux , hooks

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,14 +1,16 @@
-import React,{Component} from'react';
-export default class ItemStatusFilter extends Component{
-    buttons = [
+import React from'react';
+const ItemStatusFilter =(props)=>{
+   const buttons = [
         {name:'all',label:'All'},
         {name:'active',label:'Active'},
         {name:'done',label:'Done'},
+       
     ]
 
-    render(){
-        const {filter,onFilterChange} = this.props;
-        const buttons = this.buttons.map(({name,label})=>{
+    const {filter,onFilterChange} = props;
+
+        const Button = buttons.map(({name,label})=>{
+
         const isActive = filter === name;
         const clazz = isActive ? 'btn-info' : 'btn-outline-secondary'
             return (
@@ -17,11 +19,13 @@ export default class ItemStatusFilter extends Component{
                 onClick={()=>onFilterChange(name)}>{label}
                 </button>
             )
-        })
+        });
+
         return (
             <div className="btn-group">
-                {buttons}
+                {Button}
             </div>
         )
-    }
-}
+};
+
+export default ItemStatusFilter
